2b90b89b9be9cb8c1d391dc6f05a0860e6287657
limited-manan <manankathuria03+1@gmail.com>
2025-09-04 20:37:12 +0530

    Add EJS-Mate for layout support and update campground model with image and price fields

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const ejsMate = require('ejs-mate')
 const Campground = require('./models/campground')
 const methodOverride = require('method-override')
 
@@ -14,6 +15,7 @@ db.once('open',() =>{
 const app = express();
 const path = require('path');
 
+app.engine('ejs',ejsMate)
 app.set('view engine', 'ejs')
 app.set('views',path.join(__dirname,'views'))
 
--- seeds/index.js
+++ seeds/index.js
@@ -19,9 +19,13 @@ const seedDB = async() =>{
     // await c.save();    
     for(let i = 0;i<50;i++){
         const random1000 = Math.floor(Math.random()*1000);
+        const price = Math.floor(Math.random() * 20) +10
         const camp = new Campground({
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image: 'https://picsum.photos/400?random=${Math.random()}',
+            description: 'This is just a random text for the campground and it has no relation with our data this is just to test our site',
+            price
         })
         await camp.save();
     }
