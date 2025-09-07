0681ba406fda1113ff475009e7b6fc7c42bbca3b
limited-manan <manankathuria03+1@gmail.com>
2025-09-07 16:28:58 +0530

    Error handling & Adding Joi package

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const Campground = require('./models/campground')
 const methodOverride = require('method-override')
 const catchAsync = require('./utils/catchAsync')
 const ExpressError = require('./utils/ExpressError')
+// const Joi = require('joi')
+const {campgroundSchema} = require('./schemas')
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp')
 const db = mongoose.connection;
@@ -23,6 +25,17 @@ app.set('views', path.join(__dirname, 'views'))
 
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
+
+const validateCampground = (req,res,next) =>{
+        const {error} = campgroundSchema.validate(req.body)
+        if(error){
+            const msg = error.details.map(el => el.message).join(',')
+            throw new ExpressError(msg,400)
+        } else{
+            next();
+        }
+        // console.log(result)
+}
 
 app.get('/', (req, res) => {
     res.render('home')
@@ -43,10 +56,10 @@ app.get('/campgrounds/new', (req, res) => {
     res.render('campgrounds/new')
 })
 
-app.post('/campgrounds', catchAsync(async (req, res,next) => {
+app.post('/campgrounds', validateCampground, catchAsync(async (req, res,next) => {
     // try {
         // res.send(req.body)
-        if(!req.body.campground) throw new ExpressError('Invalid Campground Data',400)
+        // if(!req.body.campground) throw new ExpressError('Invalid Campground Data',400)
         const campground = new Campground(req.body.campground)
         await campground.save()
         res.redirect(`/campgrounds/${campground._id}`)
@@ -65,7 +78,7 @@ app.get('/campgrounds/:id/edit', catchAsync(async (req, res) => {
     res.render('campgrounds/edit', { campground })
 }))
 
-app.put('/campgrounds/:id', catchAsync(async (req, res) => {
+app.put('/campgrounds/:id', validateCampground, catchAsync(async (req, res) => {
     const { id } = req.params;
     // res.send('It worked!!')
     const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground })
