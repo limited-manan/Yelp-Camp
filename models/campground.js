6cf1f0d80ba62e02678566ed84d4ce2843900c04
limited-manan <manankathuria03+1@gmail.com>
2025-09-18 17:36:36 +0530

    Adding Fancy Cluster Map

--- models/campground.js
+++ models/campground.js
@@ -8,12 +8,13 @@ const { coordinates } = require('@maptiler/client');
 const ImageSchema = new Schema({
     url: String,
     filename: String
-
 })
 
 ImageSchema.virtual('thumbnail').get(function(){
     return this.url.replace('/upload', '/upload/w_200')
 })
+
+const opts = { toJSON: { virtuals: true} };
 
 const CampgroundSchema = new Schema({
     title: String,
@@ -41,8 +42,15 @@ const CampgroundSchema = new Schema({
             type: Schema.Types.ObjectId,
             ref: 'Review'
         }
-    ]
+    ],
+},opts)
+
+CampgroundSchema.virtual('properties.popUpMarkup').get(function(){
+    return `
+    <strong><a href="/campgrounds/${this._id}">${this.title}</a><strong>
+    <p>${this.description.substring(0,20)}...</p>`
 })
+
 
 CampgroundSchema.post('findOneAndDelete', async function (doc) {
     // console.log('DELETED!!')
