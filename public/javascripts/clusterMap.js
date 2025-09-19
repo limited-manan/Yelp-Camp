049ef748e7e9f37746034c254ce8319c9fb2a440
limited-manan <manankathuria03+1@gmail.com>
2025-09-19 14:52:53 +0530

    Final Commit For Deployment

--- public/javascripts/clusterMap.js
+++ public/javascripts/clusterMap.js
@@ -3,7 +3,7 @@ maptilersdk.config.apiKey = maptilerApiKey;
 const map = new maptilersdk.Map({
     container: 'cluster-map',
     style: maptilersdk.MapStyle.BRIGHT,
-    center: [-103.59179687498357, 40.66995747013945],
+    center: [80,23],
     zoom: 3.5
 });
 
